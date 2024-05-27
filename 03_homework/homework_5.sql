61cbfec30a9580f26e74ff4c6c669bd174881d88
Monica L. Rojas-Pena <monicarojasp@gmail.com>
2024-05-27 01:42:13 -0400

    Solved Homework_5

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -1,32 +1,3 @@
--- String manipulations
-/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
-These are separated from the product name with a hyphen. 
-Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
-Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
-
-| product_name               | description |
-|----------------------------|-------------|
-| Habanero Peppers - Organic | Organic     |
-
-Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
-
-
-
-/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
-
--- UNION
-/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
-
-HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
-2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
-"best day" and "worst day"; 
-3) Query the second temp table twice, once for the best day, once for the worst day, 
-with a UNION binding them. */
-
-
 
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
@@ -39,7 +10,30 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+DROP TABLE IF EXISTS vendor_inventory_summary;
 
+CREATE TEMP TABLE vendor_inventory_summary AS
+
+SELECT DISTINCT vendor_id, product_id, original_price
+FROM vendor_inventory;
+
+DROP TABLE IF EXISTS vendor_inventory_names;
+
+CREATE TEMP TABLE vendor_inventory_names AS
+
+SELECT vis.*, vendor_name, product_name
+FROM vendor_inventory_summary vis
+INNER JOIN vendor v
+					ON vis.vendor_id = v.vendor_id
+INNER JOIN product p
+					ON vis.product_id = p.product_id;
+
+SELECT vendor_name, product_name, sum(per_product)
+FROM
+(SELECT vendor_name, product_name, customer_id, original_price*5 as per_product
+FROM vendor_inventory_names  
+CROSS JOIN customer)
+GROUP BY vendor_name, product_name;
 
 -- INSERT
 /*1.  Create a new table "product_units". 
@@ -47,12 +41,18 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
 
+CREATE TABLE product_units AS 
+		SELECT *, CURRENT_TIMESTAMP AS snapshot_timestamp
+		FROM product
+		WHERE product_qty_type LIKE 'unit';
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-
+INSERT INTO product_units
+VALUES(7, 'Apple Pie', '10"', 3, 'unit', CURRENT_TIMESTAMP);
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
@@ -60,6 +60,9 @@ This can be any product you desire (e.g. add another record for Apple Pie). */
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
 
+DELETE FROM product_units
+WHERE product_id = 7 AND snapshot_timestamp IN ( SELECT min(snapshot_timestamp) 
+																								 FROM product_units);
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -77,5 +80,44 @@ Third, SET current_quantity = (...your select statement...), remembering that WH
 Finally, make sure you have a WHERE statement to update the right row, 
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
+
+ALTER TABLE product_units
+ADD current_quantity INT;
+
+DROP TABLE IF EXISTS last_quantity;
+
+CREATE TEMP TABLE last_quantity AS
+
+SELECT vi.product_id, vi.market_date, vi.quantity
+FROM vendor_inventory vi
+INNER JOIN (
+    SELECT product_id, MAX(market_date) AS max_market_date
+    FROM vendor_inventory
+    GROUP BY product_id
+) AS max_dates
+ON vi.product_id = max_dates.product_id
+AND vi.market_date = max_dates.max_market_date;
+
+
+DROP TABLE IF EXISTS with_quantity;
+
+CREATE TEMP TABLE with_quantity AS
+
+SELECT pu.*, quantity
+FROM product_units pu
+LEFT JOIN last_quantity lq
+					ON pu.product_id = lq.product_id;
+
+DROP TABLE IF EXISTS coal_quantity;
+
+CREATE TEMP TABLE coal_quantity AS
+				
+SELECT *
+,coalesce(quantity, 0) as no_null_quantity
+FROM with_quantity;
+
+UPDATE product_units
+SET current_quantity = (SELECT no_null_quantity FROM coal_quantity
+WHERE product_units.product_id = coal_quantity.product_id);
 
 
